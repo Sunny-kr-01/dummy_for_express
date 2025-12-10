c5c6d9a5b971f656b9ba1d90cfefc2034dfdf690
Sunny-kr-01 <2405984@kiit.ac.in>
2025-12-11 00:33:18 +0530

    http removed

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express= require('express')
-const http=require('http')
 const app =express();
 
 app.get('/',(req,res)=>{
     res.send("Hello, This is the home page")
 })
-app.get('/home',(req,res)=>{
-    res.send("Hello, This is the about page")
+app.get('/about',(req,res)=>{
+    res.send("Hello, This is the about page" + ` Hey ${req.query.name}` + ' you are ' + req.query.age)
 })
 
-const server=http.createServer(app)
+//const server=http.createServer(app)
 
-server.listen(67,()=>{
-    console.log("Serevr is listening to port 67")
+//we don't even need http
+
+//server.listen(67,()=>{
+//    console.log("Serevr is listening to port 67")
+//})
+
+app.listen(67,()=>{
+    console.log("App is listening on port 67")
 })
